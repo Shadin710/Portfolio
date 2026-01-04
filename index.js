40bde1168e921a9660e0f682aaa3a0747a2d359a
Shadin710 <shadin710@gmail.com>
2026-01-04 22:14:59 +1030

    Added form functionality

--- index.js
+++ index.js
@@ -458,3 +458,23 @@ if ('IntersectionObserver' in window) {
         lazyAnimationObserver.observe(el);
     });
 }
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const myForm = event.target;
+  const formData = new FormData(myForm);
+  
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => {
+      // Hide the form and show the feedback
+      myForm.style.display = "none";
+      document.getElementById("form-feedback").style.display = "block";
+    })
+    .catch((error) => alert(error));
+};
+
+document.querySelector("#portfolio-form").addEventListener("submit", handleSubmit);
